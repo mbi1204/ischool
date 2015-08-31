d749d6ab99ec99008ae60d6d0d72cfb71c3c6891
Alejandro Estrada <alestra55@hotmail.com>
2015-08-31 11:36:17 -0500

    Envio los servicios de grupo

---
+++ iSchool/src/main/java/com/sinergitec/ischool/service/ct/ctGrupoService.java
@@ -0,0 +1,12 @@
+package com.sinergitec.ischool.service.ct;
+
+import com.sinergitec.ischool.model.ct.ctGrupo;
+
+public interface ctGrupoService {
+	
+	public void add_ctGrupo(ctGrupo obj_Grupo);
+	
+	public void update_ctGrupo(ctGrupo obj_ctGrupo);
+	
+	public void remove_ctGrupo(int id);
+}
