da3e23d6d41fdc11cdbd26e310ae00355f2c974a
Bogar Elias <bhogar.95@gmail.com>
2015-08-31 11:39:21 -0500

    OK

---
+++ iSchool/src/main/java/com/sinergitec/ischool/model/ct/Automovil.java
@@ -0,0 +1,16 @@
+package com.sinergitec.ischool.model.ct;
+
+public class Automovil {
+	
+	private String color;
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+
+}
