33ac1cb28a2024e860e7bb8d35edcb9c8650a1bc
mbi1204 <mbi1204@hotmial.com>
2015-09-02 10:23:17 -0500

    ok

--- iSchool/src/main/java/com/sinergitec/ischool/model/ct/ctEmpleado.java
+++ iSchool/src/main/java/com/sinergitec/ischool/model/ct/ctEmpleado.java
@@ -1,8 +1,10 @@
 package com.sinergitec.ischool.model.ct;
 
 public class ctEmpleado {
-	
+
 	private String cNombre;
+
+	private String iEdad;
 
 	public String getcNombre() {
 		return cNombre;
@@ -11,7 +13,13 @@ public class ctEmpleado {
 	public void setcNombre(String cNombre) {
 		this.cNombre = cNombre;
 	}
-	
-	
+
+	public String getiEdad() {
+		return iEdad;
+	}
+
+	public void setiEdad(String iEdad) {
+		this.iEdad = iEdad;
+	}
 
 }
