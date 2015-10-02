a31729ab8b4a83143814bf2dd23c409fee1ca3cd
LuisMarez <langel.marez@sinergitec.mx>
2015-10-02 14:01:29 -0500

    reporte

---
+++ iSchool/src/main/webapp/resources/js/repAlumnoGrupo.js
@@ -0,0 +1,50 @@
+function carga_repAlumno(viIdGrupo) {
+
+	
+
+	$.ajax({
+		type : "GET",
+		url : "repAlumnoGrupo/getAlumnos",
+		dataType : "json",
+		contentType : "application/json; charset=utf-8",
+		data : {
+			iIdGrupo : viIdGrupo
+		},
+		success : function(data, textStatus, jqXHR) {
+			
+			$("#mytable> tbody").empty();
+			
+			for ( var item in data) {
+				$('#mytable > tbody').append(
+						'<tr>'
+						+ '<td>' + data[item].iIdAlumno + '</td>'  
+						+ '<td>' + data[item].cNombre + '</td>'
+						+ '<td>' + data[item].cApellido + '</td>'
+						+ '<td>' + data[item].dtFechaNac + '</td>'
+						+ '<td>' + data[item].cCalle + '</td>'
+						+ '<td>' + data[item].cNumExt + '</td>'
+						+ '<td>' + data[item].cNumInt + '</td>'
+						+ '<td>' + data[item].cColonia + '</td>'
+						+ '<td>' + data[item].cCP + '</td>'
+						+ '<td>' + data[item].cMunicipio + '</td>'
+						+ '<td>' + data[item].cEstado + '</td>'
+						+ '<td>' + data[item].cTel + '</td>'						
+						+ '</tr>');
+
+			}
+
+
+			
+
+		},
+		error : function() {
+			alert("erro al ejecutar el BuscaMenu" + textStatus);
+		}
+
+	});
+
+}
+
+
+
+
